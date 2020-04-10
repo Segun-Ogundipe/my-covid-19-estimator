b5a0d62c6c654ae3c7f22c40a5ddea079de43f7e
Segun Ogundipe <segunogundipe@witts-stratts.com>
2020-04-10 18:01:22 +0100

    implement challenge 3

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import '@babel/polyfill';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import X2JS from 'x2js';
+import fs from 'fs';
+import path from 'path';
+import covid19ImpactEstimator from './estimator';
+
+// Create global app object
+const app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, '../access.log'),
+  { flags: 'a' }
+);
+app.use(
+  morgan(':method  :url  :status  :response-time ms', {
+    stream: accessLogStream
+  })
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/api/v1/on-covid-19', (req, res) => {
+  const estimate = covid19ImpactEstimator(req.body);
+  res.send(estimate);
+});
+
+app.post('/api/v1/on-covid-19/json', (req, res) => {
+  const estimate = covid19ImpactEstimator(req.body);
+  res.send(estimate);
+});
+
+app.post('/api/v1/on-covid-19/xml', (req, res) => {
+  const xml = new X2JS();
+  const estimate = xml.js2xml(covid19ImpactEstimator(req.body));
+
+  res.set('Content-Type', 'application/xml');
+  res.send(estimate);
+});
+
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  const log = fs.readFileSync(path.join(__dirname, '../access.log'), 'utf8');
+
+  res.set('Content-Type', 'text/plain');
+  res.send(log);
+});
+
+// finally, let's start our server...
+const server = app.listen(process.env.PORT || 3000, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+export default app;
